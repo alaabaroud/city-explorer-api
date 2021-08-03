2760b864514ac75775e282bc37ca94bff28246f0
alaabaroud <alaabaroud783@gmail.com>
2021-08-03 20:47:56 +0300

    backend side

--- server.js
+++ server.js
@@ -5,18 +5,43 @@ const  weatherData =  require ('./data/weather.json');
 const cors = require('cors');
 server.use(cors()); 
 require('dotenv').config();
+
 const PORT =process.env.PORT
+
 // http://localhost:3001/weather
-server.get('/weather',(req,res) => {
-    let wheatherUbdate=weatherData.find(item=>{
-        return [item.searchQuery,item.lat,item.long];
-    })
-    // console.log(wheatherUbdate);
-    res.send(wheatherUbdate);
-  })
-  server.get('*', (req, res)=>{
-      res.status(404).send('page not found');
-  })
+
+
+server.get('/weather',WeatherFunction);
+server.use('*', (req, res) => res.status(404).send('page not found'));
+
+
+ function WeatherFunction (req,res){
+  let searchQuery = req.query.searchQuery;
+         const city= weatherData.find(city=> city.city_name.toLocaleLowerCase()=== searchQuery.toLocaleLowerCase());
+if (city != undefined){
+const arr = city.data.map(day => new update(day));
+res.status(200).send(arr);
+
+}
+else {
+errorHandler(res);
+}
+ }
+ function errorHandler(err) {
+  response.status(500).send('err');
+}
+   
+
+
+
+
+function update(day) {
+  this.date = day.valid_date
+  this.description = day.weather.description
+}
+
+
+
     server.listen(PORT,()=>{
       console.log(`I am listening on port ${PORT}`);
     })
