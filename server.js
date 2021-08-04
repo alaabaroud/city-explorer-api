ba452772c5ad1c29894f495c0c22328e02d0ae20
alaabaroud <alaabaroud783@gmail.com>
2021-08-04 07:54:48 +0300

    lab8 faild

--- server.js
+++ server.js
@@ -8,37 +8,56 @@ require('dotenv').config();
 
 const PORT =process.env.PORT
 
-// http://localhost:3001/weather
+// http://localhost:3001/test
+server.get('/test',(req,res) => {
+  res.send('test route');
+});
 
 
-server.get('/weather',WeatherFunction);
-server.use('*', (req, res) => res.status(404).send('page not found'));
 
 
- function WeatherFunction (req,res){
-  let searchQuery = req.query.searchQuery;
-         const city= weatherData.find(city=> city.city_name.toLocaleLowerCase()=== searchQuery.toLocaleLowerCase());
-if (city != undefined){
-const arr = city.data.map(day => new update(day));
-res.status(200).send(arr);
+// http://localhost:3001/getmovie
+server.get('/getmovie',(req,res) => {
+  res.send('test getMovie');
+});
 
-}
-else {
-errorHandler(res);
-}
- }
- function errorHandler(err) {
-  response.status(500).send('err');
-}
+
+// http://localhost:3001/
+server.get('*',(req,res) => {
+  res.status(404).send('route not found');
+});
+// // http://localhost:3001/weather
+
+
+// server.get('/weather',WeatherFunction);
+// server.use('*', (req, res) => res.status(404).send('page not found'));
+
+
+//  function WeatherFunction (req,res){
+//   let searchQuery = req.query.searchQuery;
+//          const city= weatherData.find(city=> city.city_name.toLocaleLowerCase()=== searchQuery.toLocaleLowerCase());
+// if (city != undefined){
+// const arr = city.data.map(day => new update(day));
+// res.status(200).send(arr);
+
+// }
+// else {
+// errorHandler(res);
+// }
+//  }
+//  function errorHandler(err) {
+//   response.status(500).send('err');
+// }
    
 
 
 
+// function update(day) {
+//   this.date = day.valid_date
+//   this.description = day.weather.description
+// }
 
-function update(day) {
-  this.date = day.valid_date
-  this.description = day.weather.description
-}
+// //https://api.themoviedb.org/3/movie/550?api_key=4b557fce5a80bc4685790b5ceb6c8804
 
 
 
